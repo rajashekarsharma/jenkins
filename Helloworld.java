d15b59cf2147a091f59dae4ed028cc5cc9658beb
rajashekarsharma <rajashekarsharma@gmail.com>
2023-06-21 10:52:51 +0530

    Java program Helloworld

---
+++ Helloworld.java
@@ -0,0 +1,8 @@
+public class Helloworld{
+    public static void main(String [] s){
+        System.out.println("Hellow World");
+        for (int i=0;i<=10;i++){
+           System.out.println(i); 
+        }
+    }
+}
